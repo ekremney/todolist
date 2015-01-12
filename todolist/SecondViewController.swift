cba53e8137032cbfa3a60c6a549abc985228d7e2
Ekrem Doğan <Ekrem@Ekrem-MacBook-Pro.local>
2015-01-12 09:33:24 +0200

    Initial Commit

---
+++ todolist/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  todolist
+//
+//  Created by Ekrem Doğan on 12.01.2015.
+//  Copyright (c) 2015 Ekrem Doğan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
